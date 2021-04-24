a98a5d0850c79f493a2c2bf3e7cb532268a99c9c
ewa.suknarowska <ewa.suknarowska@gmail.com>
2021-04-24 20:18:26 +0200

    kaggle score 5.03169

--- utils/learning.py
+++ utils/learning.py
@@ -4,7 +4,26 @@ from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import KFold
 import numpy as np 
 
-def run_cv(model, X, y, folds=4, target_log=False,cv_type=KFold, success_metric=mean_absolute_error):
+def ln_exp_obj(preds,dtrain):
+    labels = dtrain.get_label()
+    x = preds - labels
+    exp_2x = np.exp(2*x)
+    grad = (exp_2x -1 )/ (exp_2x + 1 )
+    hess = (4 * exp_2x) / (exp_2x + 1 )**2 
+    return grad, hess
+
+def mae_log_eval(y_log_pred,dtran):
+    y_log_true = dtrain.get_label()
+    y_true = np.exp(y_log_true)
+    y_pred = np.exp(y_log_pred)
+    return 'mea', mean_absolute_error(y_true,y_pred)
+
+# def train_and_predict(model, X, y):
+#     model.fit(X, y)
+#     y_pred = model.predict(X)
+#     return mean_absolute_error(y, y_pred)
+
+def run_cv(model, X, y, folds=4, target_log=False,cv_type=KFold):
     cv = cv_type(n_splits=folds)
     
     scores = []
@@ -22,7 +41,7 @@ def run_cv(model, X, y, folds=4, target_log=False,cv_type=KFold, success_metric=
             y_pred = np.exp(y_pred)
             y_pred[y_pred < 0] = 0 #czasem może być wartość ujemna
 
-        score = success_metric(y_test, y_pred)
+        score = mean_absolute_error(y_train,y_pred)
         scores.append( score )
         
     return np.mean(scores), np.std(scores)
@@ -72,3 +91,19 @@ def plot_learning_curve(model, X, y, ylim=None, cv=None,
 
     plt.legend(loc="best")
     return plt
+
+
+def plot_feature_importances(model, feats, limit = 20):
+    importances = model.feature_importances_
+
+    indices = np.argsort(importances)[::-1][:limit]
+
+    plt.figure(figsize=(20, 8))
+    plt.title("Feature importances")
+
+    print(range(limit))
+    print(importances[indices])
+    plt.bar(range(limit), importances[indices])
+    plt.xticks(range(limit), [feats[i] for i in indices], rotation='vertical')
+    plt.show()
+  
